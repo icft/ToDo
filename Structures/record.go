491ab052119c9a7cfc6cff6ad5378a6a7d299b76
vlhod <40525571+icft@users.noreply.github.com>
2021-08-14 12:01:28 +0300

    Delete record.go

--- Structures/record.go
+++
@@ -1,9 +0,0 @@
-package Structures
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Records struct {
-	ID      primitive.ObjectID `json:"id,omitempty"`
-	Problem string             `json:"problem,omitempty"`
-	Status  bool               `json:"status,omitempty"`
-}
