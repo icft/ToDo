20e9869987cc4144c0ec924b492522e4df042ea8
vlhod <40525571+icft@users.noreply.github.com>
2021-08-14 13:48:51 +0300

    Delete Structures directory

--- Structures/note.go
+++
@@ -1,9 +0,0 @@
-package Structures
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Note struct {
-	ID      primitive.ObjectID `json:"id,omitempty"`
-	Problem string             `json:"problem,omitempty"`
-	Status  bool               `json:"status,omitempty"`
-}
